e5bfaa25a523f9dd8d4da190240ac0af0c876019
yubinm <yubinmoon0910@gmail.com>
2022-12-28 15:57:25 -0500

    bar + some text
    Not completely sure if I can edit this branch later but I made a nav bar? If edits are possible I'll do more ig

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,19 +1,44 @@
 import React, { Component } from 'react';
-
+<link href="https://fonts.cdnfonts.com/css/archivo" rel="stylesheet"></link>
 const NavBar = (props) => {
+    const mystyle = {
+        
+        position: "absolute",
+        width: "100%",
+        height: "5%",
+        left: "0px",
+        top: "20px",
+
+        background: "#2A3065",
+        boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.3)",
+        borderRadius: "50px"
+      };
+    const words = {
+        
+        fontFamily: "Archivo, sans-serif",
+        fontStyle: "bold",
+        fontWeight: "400",
+        fontSize: "20px",
+        lineHeight: "61px",
+        color: "#FFFFFF",
+        padding:"10%",
+        
+        
+
+    }
     return (
+        
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">Navbar</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <a  className="navbar-brand" href="#">Navbar</a>
+            <button  className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div style={mystyle} className="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div className="navbar-nav">
-                    <a className="nav-link active" href='/home'>Home <span class="sr-only">(current)</span></a>
-                    <a className="nav-link" href="/requests">Requests</a>
-                    <a className="nav-link" href="/profile">Profile</a>
-                    <a className="nav-link" href="/about">About TIF3</a>
-                    <a className="nav-link" onClick={props.onReturnHome}>Logout</a>
+                    <a style={words} className="nav-link" href="/requests">Requests</a>
+                    <a style={words} className="nav-link" href="/profile">Profile</a>
+                    <a style={words} className="nav-link" href="/about">About TIF3</a>
+                    <a style = {words} className="nav-link" onClick={props.onReturnHome}>Logout</a>
                 </div>
             </div>
         </nav>
